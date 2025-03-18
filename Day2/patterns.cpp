06fa74269eed36a0f03024c1a5093330d9ee8c21
Suravi Jha <jhasuravi06@gmail.com>
2025-03-18 20:38:40 +0530

    Update patterns.cpp

--- Day2/patterns.cpp
+++ Day2/patterns.cpp
@@ -125,3 +125,15 @@ void pattern10(int n) {
         cout << endl;
     }
 }
+
+void pattern11(int n) {
+    for (int i = 0; i < n; i++) {
+        int num = 1;
+        if (i % 2 == 1) num = 0;
+        for (int j = 0; j <= i; j++) {
+            cout << num << " ";
+            num = 1 - num;
+        }
+        cout << endl;
+    }
+}
