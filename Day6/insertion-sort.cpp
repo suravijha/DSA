75b7d99c9c4416d39ce10af1f3ce70fc00e5cf16
Suravi Jha <jhasuravi06@gmail.com>
2025-03-28 00:04:00 +0530

    Create insertion-sort.cpp

---
+++ Day6/insertion-sort.cpp
@@ -0,0 +1,12 @@
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+    }
+}
