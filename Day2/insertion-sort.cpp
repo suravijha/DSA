d0343afbfe1a0e98fdc01877c81c863f7c6acfee
Suravi Jha <jhasuravi06@gmail.com>
2025-05-26 19:14:27 +0530

    Create insertion-sort.cpp

---
+++ Day2/insertion-sort.cpp
@@ -0,0 +1,16 @@
+// Given an array of integers called nums, sort the array in non-decreasing order using the insertion sort algorithm and return the sorted array.
+// A sorted array in non-decreasing order is an array where each element is greater than or equal to all preceding elements in the array.
+
+vector<int> insertionSort(vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 1; i < n; i++) {
+        int j = i;
+        while (j > 0 && nums[j - 1] > nums[j]) {
+            int temp = nums[j - 1];
+            nums[j - 1] = nums[j];
+            nums[j] = temp;
+            j--;
+        }
+    }
+    return nums;
+}
