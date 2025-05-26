d4bfc2e63d9aa77c9ac5aaf0d70291a1e4077b8c
Suravi Jha <jhasuravi06@gmail.com>
2025-05-26 15:20:44 +0530

    Create selection-sort.cpp

---
+++ Day2/selection-sort.cpp
@@ -0,0 +1,18 @@
+// Given an array of integers nums, sort the array in non-decreasing order using the selection sort algorithm and return the sorted array.
+// A sorted array in non-decreasing order is an array where each element is greater than or equal to all previous elements in the array.
+
+vector<int> selectionSort(vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i; j < n; j++) {
+            if (nums[j] < nums[min]) {
+                min = j;
+            }
+        }
+        int temp = nums[min];
+        nums[min] = nums[i];
+        nums[i] = temp;
+    }
+    return nums;
+}
