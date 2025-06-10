a23e0fc4dc9d18afa3441f4df026895e705fa224
Suravi Jha <jhasuravi06@gmail.com>
2025-06-10 14:53:20 +0530

    Create segregate-0s-1s.cpp

---
+++ Day7/segregate-0s-1s.cpp
@@ -0,0 +1,14 @@
+void segregate0and1(vector<int> &arr) {
+    int i = 0, j = arr.size() - 1;
+    while (i < j) {
+        if (arr[i] == 0) {
+            i++;
+        } else if (arr[j] == 1) {
+            j--;
+        } else {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+}
