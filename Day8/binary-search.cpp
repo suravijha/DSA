7a410fdac6366130e496a03661ef33b141b772be
Suravi Jha <jhasuravi06@gmail.com>
2025-06-11 20:28:12 +0530

    Create binary-search.cpp

---
+++ Day8/binary-search.cpp
@@ -0,0 +1,17 @@
+int binarySearch(int arr[], int n, int key) {
+    int start = 0;
+    int end = n - 1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
