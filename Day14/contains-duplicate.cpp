cda1c70da541c64e3a14df11ef0e20bafb3b1a1b
Suravi Jha <jhasuravi06@gmail.com>
2025-06-19 21:24:15 +0530

    Create contains-duplicate.cpp

---
+++ Day14/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+// LeetCode 217
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] == nums[i]) 
+                return true;
+        }
+        return false;
+    }
+};
