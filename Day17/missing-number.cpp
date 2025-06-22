c403cd49e2086950b70583a9d597063a99cb33c2
Suravi Jha <jhasuravi06@gmail.com>
2025-06-22 14:21:22 +0530

    Create missing-number.cpp

---
+++ Day17/missing-number.cpp
@@ -0,0 +1,16 @@
+// LeetCode 268
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int ans = nums.size();
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans += i - nums[i];
+        }
+        return ans;
+    }
+};
