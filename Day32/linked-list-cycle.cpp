d493933dd65fda3299ca1118f923f5192bd52324
Suravi Jha <jhasuravi06@gmail.com>
2025-07-07 18:50:26 +0530

    Create linked-list-cycle.cpp

---
+++ Day32/linked-list-cycle.cpp
@@ -0,0 +1,29 @@
+// LeetCode 141
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
