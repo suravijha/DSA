5d4548473262a619dc0dc09c51e4e3a9d3e470ea
Suravi Jha <jhasuravi06@gmail.com>
2025-07-21 20:29:43 +0530

    Create binary-tree-postorder-traversal.cpp

---
+++ Day46/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,35 @@
+// LeetCode 145
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    void solve(TreeNode *root, vector<int> &ans)
+    {
+        if (root)
+        {
+            solve(root->left, ans);
+            solve(root->right, ans);
+            ans.push_back(root->val);
+        }
+    }
+
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
