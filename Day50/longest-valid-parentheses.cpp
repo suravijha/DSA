d21acffa3a6fcf4ec27d0cb4b088b6295807b9d7
Suravi Jha <jhasuravi06@gmail.com>
2025-07-25 17:20:30 +0530

    Create longest-valid-parentheses.cpp

---
+++ Day50/longest-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+// LeetCode 32
+// Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+class Solution
+{
+public:
+    int longestValidParentheses(string s)
+    {
+        stack<int> st;
+        st.push(-1);
+        int max_len = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if (st.empty())
+                {
+                    st.push(i);
+                }
+                else
+                {
+                    max_len = max(max_len, i - st.top());
+                }
+            }
+        }
+
+        return max_len;
+    }
+};
