ea5306888996c86c86e7202a520f30e98c0fd4f0
Suravi Jha <jhasuravi06@gmail.com>
2025-08-03 22:46:20 +0530

    Create merge-in-between-linked-lists.cpp

---
+++ Day59/merge-in-between-linked-lists.cpp
@@ -0,0 +1,41 @@
+// LeetCode 1669
+// You are given two linked lists: list1 and list2 of sizes n and m respectively.
+// Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *mergeInBetween(ListNode *list1, int a, int b, ListNode *list2)
+    {
+        ListNode *curr = list1;
+        int i = 0;
+        while (i < a - 1)
+        {
+            curr = curr->next;
+            i++;
+        }
+        ListNode *head = curr;
+        while (i <= b)
+        {
+            curr = curr->next;
+            i++;
+        }
+        head->next = list2;
+        while (list2->next)
+        {
+            list2 = list2->next;
+        }
+        list2->next = curr;
+        return list1;
+    }
+};
