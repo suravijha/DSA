1a1a06fe08b13c4ed86da0ffdb98bd14d463c1c4
Suravi Jha <jhasuravi06@gmail.com>
2025-08-14 22:51:28 +0530

    Create implement-stack-using-queues.cpp

---
+++ Day70/implement-stack-using-queues.cpp
@@ -0,0 +1,49 @@
+// LeetCode 225
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+class MyStack
+{
+private:
+    queue<int> q;
+
+public:
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop()
+    {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+
+    int top()
+    {
+        return q.front();
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
