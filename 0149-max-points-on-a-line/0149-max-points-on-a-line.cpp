04ddd4ce6f0343945886a90f35d4c4e1166be891
Suravi Jha <jhasuravi06@gmail.com>
2026-08-13 20:24:12 +0530

    Time: 19 ms (79.89%), Space: 15.5 MB (83.36%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        if (n <= 2)
+            return n;
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            map<pair<int, int>, int> slope;
+            int currentMax = 0;
+
+            for (int j = i + 1; j < n; j++) {
+                int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
+
+                if (dx == 0) {
+                    dy = 1;
+                } else if (dy == 0) {
+                    dx = 1;
+                } else {
+                    int g = gcd(abs(dx), abs(dy));
+                    dx /= g;
+                    dy /= g;
+                }
+
+                if (dx < 0) {
+                    dx *= -1;
+                    dy *= -1;
+                } 
+
+                slope[{dx, dy}]++;
+                currentMax = max(currentMax, slope[{dx, dy}]);
+            }
+
+            ans = max(ans, currentMax + 1);
+        }
+
+        return ans;
+    }
+};
